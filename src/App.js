ed9d59988f53e79a05722b18319c7179d2b37abe
Rick Obi <sanunes.ricardo@gmail.com>
2020-11-08 17:58:54 -0500

    BACK | Added basic fetching of backend resourses: Battles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super();
+    this.state = {
+      list: true,
+      card: false,
+      battles: [],
+      battle: {}
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3001/battles/list")
+    .then(res => res.json())
+    .then( resJson => {
+      this.setState({ battles: resJson.data });
+    });
+  }
+
+  showCard = id => {
+    fetch(`http://localhost:3001/battles/${id}`)
+    .then(res => res.json())
+    .then(resJson => {
+      this.setState({ battle: resJson.data });
+    });
+    this.setState({
+      list: false,
+      card: true
+    });
+  }
+
+  showList = () => {
+    this.setState({
+      card: false,
+      list: true
+    });
+  }
+
+  render() {
+    return(
+      <div className="container">
+        {this.state.list ? (
+          <div className="list-group">
+            {this.state.battles.map(battle => (
+              <li
+                onClick={() => this.showCard(battle._id)}
+                className="list-group-item list-group-item-action"
+              >
+                {battle.winner===1 ? battle.pokemon_1 : battle.pokemon_2}
+              </li>
+            ))}
+          </div>
+        ) : null }
+        {this.state.card ? (
+          <div class="card" style={{ width: "16rem" }}>
+            <div class="card-body">
+              <h5 class="card-title">{this.state.battle.winner}</h5>
+              <p class="card-text">{this.state.battle.pokemon_1}</p>
+              <p class="card-text">{this.state.battle.pokemon_2}</p>
+              <div onClick={() => this.showList()} class="btn btn-primary">
+                Back
+              </div>
+            </div>
+          </div>
+        ) : null }
+      </div>
+    );
+  }
 }
-
-export default App;
